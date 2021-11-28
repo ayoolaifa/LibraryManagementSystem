e7de7ce25d4df9218fae9667928990773ab55e6b
Ayo <ayoolaifa@gmail.com>
2021-11-28 22:52:08 +0000

    Writing To Log File Checkout v4

--- database.py
+++ database.py
@@ -30,7 +30,6 @@ def write_to_file(list_of_books):
 
 def write_to_log_file(function, date, member_id, book_id):
     f = open("logfile.txt", "r+")
-    for i in f:
-        f.write(i)
+    print("Book "+ function + "\n" + "Date: " + str(date) + "\n" + "Member ID: " + str(member_id) + "\n" + "Book ID: " + str(book_id) + "\n" + "---------------" + "\n")
     f.write("Book "+ function + "\n" + "Date: " + str(date) + "\n" + "Member ID: " + str(member_id) + "\n" + "Book ID: " + str(book_id) + "\n" + "---------------" + "\n")
     f.close()
